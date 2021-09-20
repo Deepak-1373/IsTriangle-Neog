4274c96a7d254bc352155d80a81450a4d28f6efa
Deepak Kamat <dkamat.001@gmail.com>
2021-09-20 21:50:54 +0530

    Feature: Sum of Angles functionality complete

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 var angleInput = document.querySelectorAll(".input-angle");
 var isTriangleBtn = document.querySelector("#isTriangle-btn");
 var outputDiv = document.querySelector("#output");
+
+function calculateSumOfAngle(angle1, angle2, angle3) {
+  var sumOfAngles = angle1 + angle2 + angle3;
+  return sumOfAngles;
+}
+
+function isTriangle() {
+  for (let i = 0; i < angleInput.length - 2; i++) {
+    var angle1 = Number(angleInput[i].value);
+    var angle2 = Number(angleInput[i + 1].value);
+    var angle3 = Number(angleInput[i + 2].value);
+  }
+  var sumOfAngles = calculateSumOfAngle(angle1, angle2, angle3);
+  if (sumOfAngles === 180) {
+    outputDiv.innerText = "The angles form a Triangle📐";
+  } else {
+    outputDiv.innerText = "The angles don't form a triangle❌";
+  }
+}
+isTriangleBtn.addEventListener("click", isTriangle);
