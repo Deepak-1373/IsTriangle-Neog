4ea74adb73983629f359adb0bee1bd3241e68dab
Deepak Kamat <dkamat.001@gmail.com>
2021-09-21 20:23:02 +0530

    Feature: Hypotenuse Function Complete

---
+++ components/hypotenuse.js
@@ -0,0 +1,26 @@
+var sides = document.querySelectorAll(".side-input");
+var hypotenuseBtn = document.querySelector("#hypotenuse-btn");
+var outputDiv = document.querySelector("#output");
+
+function calculateSumOfSquares(a, b) {
+  var sumOfSquares = a * a + b * b;
+  return sumOfSquares;
+}
+
+function calculateHypotenuse() {
+  var sidesValue = calculateSumOfSquares(
+    Number(sides[0].value),
+    Number(sides[1].value)
+  );
+  var hypotenuse = Math.sqrt(sidesValue);
+  console.log(hypotenuse);
+  outputDiv.innerText =
+    "The Hypotenuse of " +
+    sides[0].value +
+    " and " +
+    sides[1].value +
+    " is " +
+    hypotenuse;
+}
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);
