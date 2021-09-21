3549633669c4c3cdea486b6584448a42524b7875
Deepak Kamat <dkamat.001@gmail.com>
2021-09-21 19:42:21 +0530

    Feature: QuizForm on Triangles Complete

---
+++ components/quiz.js
@@ -0,0 +1,20 @@
+var quizForm = document.querySelector(".quiz-form");
+var submitBtn = document.querySelector("#submit-btn");
+var outputDiv = document.querySelector("#output");
+
+var correctAnswers = ["90", "rightangled"];
+
+function calculateScore() {
+  var score = 0;
+  var index = 0;
+  var formResults = new FormData(quizForm);
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  outputDiv.innerText = "Hey👋 your score is " + score;
+}
+
+submitBtn.addEventListener("click", calculateScore);
